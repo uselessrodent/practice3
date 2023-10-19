e5ffc0ab911653da1313b85a6bc0a9ab85b0a842
Vlad <vladoleynik1239@gmail.com>
2023-10-19 11:43:04 +0300

    fix 1

--- practice3/practice3.cpp
+++ practice3/practice3.cpp
@@ -1,6 +1,7 @@
 ﻿#define _USE_MATHS_DEFINITIONS
 /*#﻿define _CRT_SECURE_NO_WARNINGS*/
-
+#define M_PI 3.14159265358979323846 /* pi */
+//вариант 1
 #include <stdio.h>
 #include <math.h>
 #include <Windows.h>
@@ -12,27 +13,34 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    double x = -4.5, y = 0.75 * pow(10, -4.0);
-    double z = 0.845 * pow(10, 2), u = -55.6848;
+    double x = 14.26, y = -1.22;
+    double z = 3.5 * pow(10, -2), t = 0.564849;
     double t1, t2;
 
     printf("Введите значение x,y,z:  ");
     scanf_s("%lf %lf %lf", &x, &y, &z);
 
-    if (z > 1 || z < -1) {
-        printf("error \n"); return 0;
+    if ((x - M_PI / 6) < -1 || (x - M_PI / 6) > 1) {
+        printf("error"); return 0;
     }
-        
 
-    double t1p1 = cbrt(8 + pow(abs(x - y), 2) + 1);
-    double t1p2 = pow(x, 2) + pow(y, 2) + 2;
+    if (y < -1 || y > 1) {
+        printf("error"); return 0;
+    }
 
-    double t2p1 = exp(abs(x - y));
-    double t2p2 = pow((tan(z) * tan(z) + 1), x);
+    if ((3 - (pow(z, 2) / 5)) == 0) {
+        printf("error"); return 0;
+    }
+
+    double t1p1 = 2 * cos(x - M_PI / 6);
+    double t1p2 = 0.5 + sin(y) * sin(y);
+
+    double t2p1 = pow(z, 2);
+    double t2p2 = 3 - (pow(z, 2) / 5);
 
     t1 = t1p1 / t1p2;
-    t2 = t2p1 * t2p2;
-    double t3 = t1 - t2;
+    t2 = 1 + (t2p1 / t2p2);
+    double t3 = t1 * t2;
 
 
     printf("%lg", t3);
